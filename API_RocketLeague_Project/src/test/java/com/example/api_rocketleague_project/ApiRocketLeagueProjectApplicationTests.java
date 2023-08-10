66855de03d3ef2a6c47c832c368b0fe85f1fac8f
Jonathan Druet <jonathandruet2@gmail.com>
2023-08-10 15:19:38 +0200

    api

---
+++ API_RocketLeague_Project/src/test/java/com/example/api_rocketleague_project/ApiRocketLeagueProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_rocketleague_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRocketLeagueProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
